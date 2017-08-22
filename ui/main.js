7dd2c52de9981027c8f6745889a85a8f73e858f4
JagaPrasannaa <31217052+JagaPrasannaa@users.noreply.github.com>
2017-08-22 21:52:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="New value";
-var img=document.getElementById("madi");
+var img=document.getElementById("modi");
 img.onclick=function(){
     img.style.marginleft="100px";
 };
